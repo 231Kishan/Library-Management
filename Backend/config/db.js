82726ab949fcba794cfa52d907604e9682352e11
Kishan Tripathi <kishantiwari231@gmail.com>
2025-04-29 21:44:05 +0530

    commited

--- Backend/config/db.js
+++
@@ -1,9 +0,0 @@
-/*const mongoose = require('mongoose');
-require("dotenv").config()
-function connectDB() {
-  mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('connection error:', err));
-}
-
-module.exports = connectDB;*/
